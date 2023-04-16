0eda834bb3783aca85117ac46db791e961465c36
Kawamiya <2465644598@qq.com>
2023-04-16 06:39:36 -0400

    Todo--- add test for other 2 component

---
+++ tests/unit/AmazonCentral.spec.ts
@@ -0,0 +1,10 @@
+import { mount  } from '@vue/test-utils'
+import AmazomCentral from '@/views/Amazon.vue'
+
+describe('HelloWorld.vue', () => {
+  it('renders test', () => {
+    const wrapper = mount (AmazomCentral)
+
+    // expect( wrapper.text()).toMatch("Sign in")
+  })
+})
